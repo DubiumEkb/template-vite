0aef573db7f3885c59cc1fc841c2d28862405359
Mikhail Vostroknutov <dubium-ekb@yandex.ru>
2024-08-19 02:16:07 +0500

    Update app

--- src/app/app.tsx
+++ src/app/app.tsx
@@ -1,13 +1,13 @@
-import { Suspense, type FC } from "react"
+import type { FC } from "react"
 import { BrowserRouter } from "react-router-dom"
 import { AppRoutes } from "@/app"
 
 export const App: FC = () => {
 	return (
-		<Suspense fallback={<>Loading...</>}>
+		<>
 			<BrowserRouter>
 				<AppRoutes />
 			</BrowserRouter>
-		</Suspense>
+		</>
 	)
 }
